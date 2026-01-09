f4ad5f2df5415b916483f9d55fbcfc2a0538d3a1
mag-d-alen <maayanbenbassat@gmail.com>
2026-01-09 11:10:04 +0100

    Fix CORS configuration to support multiple frontend URLs for improved flexibility.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: process.env.FRONTEND_URL || process.env.FRONTEND_DEV_URL,
+    origin: [
+      process.env.FRONTEND_DEV_URL || '',
+      process.env.FRONTEND_URL || '',
+    ],
     credentials: true,
   });
 
