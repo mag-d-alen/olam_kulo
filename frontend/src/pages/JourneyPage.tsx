fe55e3014251afc7f86b13fe175981a2f6b3ea5d
mag-d-alen <maayanbenbassat@gmail.com>
2026-01-21 10:57:30 +0100

    Remove unnecessary default values and console logs.

--- frontend/src/pages/JourneyPage.tsx
+++ frontend/src/pages/JourneyPage.tsx
@@ -3,19 +3,6 @@ import { Map } from '../components/Map';
 
 export const JourneyPage = () => {
   const { user } = useUser();
-  console.log(user);
-  const destination = user?.destination ?? {
-    city: '',
-    country: '',
-    lat: 0,
-    lng: 0,
-  };
-  const homeCity = user?.homeCity ?? {
-    city: '',
-    country: '',
-    lat: 0,
-    lng: 0,
-  };
 
-  return <Map destination={destination} homeCity={homeCity} />;
+  return <Map destination={user?.destination!} homeCity={user?.homeCity!} />;
 };
